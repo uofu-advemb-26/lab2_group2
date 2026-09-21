f81bbfd47092d20b7c310f6ab388b7f2922066b1
Josh Martin <joshmartinmaker@gmail.com>
2026-09-21 12:41:27 -0600

    Add tests for additional non-alphabetic chars

--- test/test_console.c
+++ test/test_console.c
@@ -36,4 +36,44 @@ void test_console_notAlpha(void)
 	char actual = switch_case(c);
 
 	TEST_ASSERT_TRUE_MESSAGE(actual == expected, "Failed to return non-alphabetic char.");
+
+	c = '0';
+	expected = '0';
+	actual = switch_case(c);
+	TEST_ASSERT_TRUE_MESSAGE(actual == expected, "Failed to return numeric char.");
+
+	c = '5';
+	expected = '5';
+	actual = switch_case(c);
+	TEST_ASSERT_TRUE_MESSAGE(actual == expected, "Failed to return numeric char.");
+
+	c = '9';
+	expected = '9';
+	actual = switch_case(c);
+	TEST_ASSERT_TRUE_MESSAGE(actual == expected, "Failed to return numeric char.");
+
+	c = '!';
+	expected = '!';
+	actual = switch_case(c);
+	TEST_ASSERT_TRUE_MESSAGE(actual == expected, "Failed to return special char.");
+
+	c = ' ';
+	expected = ' ';
+	actual = switch_case(c);
+	TEST_ASSERT_TRUE_MESSAGE(actual == expected, "Failed to return whitespace char.");
+
+	c = '@';
+	expected = '@';
+	actual = switch_case(c);
+	TEST_ASSERT_TRUE_MESSAGE(actual == expected, "Failed to return special char.");
+
+	c = '3';
+	expected = '3';
+	actual = switch_case(c);
+	TEST_ASSERT_TRUE_MESSAGE(actual == expected, "Failed to return numeric char.");
+
+	c = '%';
+	expected = '%';
+	actual = switch_case(c);
+	TEST_ASSERT_TRUE_MESSAGE(actual == expected, "Failed to return special char.");
 }
